59c29c5bfb593ff31ec217f1caee604e2ac110b8
karnhao <srv33605@gmail.com>
2025-07-10 16:23:43 +0700

    update footer - add linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,13 @@ const Footer = () => (
   <footer className="footer">
     <p>© {new Date().getFullYear()} Sittipat Tapsutar • Crafted with ❤️ and React</p>
     <p>
+      <span>Visit my </span>
       <a href="https://github.com/karnhao" target="_blank" rel="noopener noreferrer">
-        Visit my GitHub
+        GitHub
+      </a>
+      <span> | </span>
+      <a href="linkedin.com/in/sittipat-tepsutar-8aa100335" target="_blank" rel="noopener noreferrer">
+        Linkedin
       </a>
     </p>
   </footer>
